b69106ab98ac8747740fefeff694d76fe2624f41
luisaapinho <40220114@esmad.ipp.pt>
2023-05-24 15:45:05 +0100

    trying to do the trash :P

--- js/views/workspaceView.js
+++ js/views/workspaceView.js
@@ -1,4 +1,24 @@
-const pause=document.querySelector("#pause").addEventListener("click",function(event){
-    event.preventDefault()
-    window.location.href = "../html/pauseScreen.html";
-})
+document.addEventListener("DOMContentLoaded", function() {
+    const pause = document.querySelector("#pause");
+    pause.addEventListener("click", function(event) {
+        event.preventDefault();
+        window.location.href = "../html/pauseScreen.html";
+    });
+  
+    const trashCan = document.querySelector("#trashCan");
+    const trashOpen = document.querySelector("#imgTrashOpen");
+    const closeBtn = document.querySelector("#closeBtn");
+    const fileImage=document.querySelector("#fileImage")
+  
+    trashCan.addEventListener("click", function() {
+        trashOpen.style.display = 'flex';
+    });
+  
+    closeBtn.addEventListener("click", function() {
+        alert("oi")
+        trashOpen.style.display = 'none';
+    });
+
+   
+  });
+  
