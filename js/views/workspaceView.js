d5e55c3193df616f88e1cb9a997118ff1e58dcbb
luisaapinho <40220114@esmad.ipp.pt>
2023-05-29 12:26:25 +0100

    FINALMENTEAJBSDASUDASDN MAPING WOKING

--- js/views/workspaceView.js
+++ js/views/workspaceView.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+$(document).ready(function() {
     const pause = document.querySelector("#pause");
     pause.addEventListener("click", function(event) {
       event.preventDefault();
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   
     const trashCan = document.querySelector("#trashCan");
-    const trashOpen = document.querySelector("#trashOpen img");
+    const trashOpen = document.querySelector("#imgTrashOpen");
     const closeBtn = document.querySelector("#closeBtn");
   
     trashCan.addEventListener("click", function() {
@@ -14,52 +14,9 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   
     closeBtn.addEventListener("click", function() {
-        alert("oooi")
       trashOpen.style.display = 'none';
     });
   
-    // Funciton that makes the imageMap responsive
-    function makeImageMapResponsive() {
-        const imgWidth = trashOpen.clientWidth;
-        const imgHeight = trashOpen.clientHeight;
-        const areas = document.querySelectorAll("#imgTrashOpen area");
-      
-        areas.forEach(function(area) {
-          const coords = area.getAttribute("coords").split(",");
-          const newCoords = [];
-      
-          for (let i = 0; i < coords.length; i++) {
-            if (i % 2 === 0) {
-              newCoords.push((coords[i] / imgWidth) * 100); // Calculates the new cordanate of X
-            } else {
-              newCoords.push((coords[i] / imgHeight) * 100); // Calculates the new cordanate of y
-            }
-          }
-      
-          area.setAttribute("coords", newCoords.join(","));
-        });
-      
-        // Adjust the new coords of the area of the closeBtn
-        const closeBtnCoords = document.querySelector("#closeBtn").getAttribute("coords").split(",");
-        const newCloseBtnCoords = [];
-        
-        for (let i = 0; i < closeBtnCoords.length; i++) {
-          if (i % 2 === 0) {
-            newCloseBtnCoords.push((closeBtnCoords[i] / imgWidth) * 100); // Calculates the new cordanate of X
-          } else {
-            newCloseBtnCoords.push((closeBtnCoords[i] / imgHeight) * 100); // Calculates the new cordanate of y
-          }
-        }
-        
-        document.querySelector("#closeBtn").setAttribute("coords", newCloseBtnCoords.join(","));
-      }
-      
-  
-    makeImageMapResponsive(); // Calls the function when the page is loaded
-  
-    window.addEventListener("resize", makeImageMapResponsive); // Calls the function when the image is resized
-  
-  
-
-  });
-  
+    // Inicializa o plugin "Responsive Image Maps"
+    $('img[usemap]').rwdImageMaps();
+});
