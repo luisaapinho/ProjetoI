a94147d11fa2d6b2686672ccd5f48825fda2ada4
Nelson Gomes <nelson.gomes22@hotmail.com>
2023-05-31 11:30:39 +0100

    testing timer

--- js/views/workspaceView.js
+++ js/views/workspaceView.js
@@ -2,3 +2,36 @@ const pause=document.querySelector("#pause").addEventListener("click",function(e
     event.preventDefault()
     window.location.href = "../html/pauseScreen.html";
 })
+
+// Set the countdown time (in minutes)
+const countdownTime = 1;
+
+// Calculate the end time
+const endTime = new Date();
+endTime.setMinutes(endTime.getMinutes() + countdownTime);
+
+// Update the countdown timer every second
+const timerInterval = setInterval(updateTimer, 1000);
+
+function updateTimer() {
+  const now = new Date();
+  const remainingTime = Math.floor((endTime - now) / 1000); // Remaining time in seconds
+
+  if (remainingTime <= 0) {
+    // Timer has reached zero
+    clearInterval(timerInterval);
+    document.getElementById('timer').textContent = '00:00';
+    alert("Acabou o tempo!Perdeu!")
+  } else {
+    // Format the remaining time as mm:ss
+    const minutes = Math.floor(remainingTime / 60);
+    const seconds = remainingTime % 60;
+    const formattedTime = `${padZero(minutes)}:${padZero(seconds)}`;
+
+    document.getElementById('timer').textContent = formattedTime;
+  }
+}
+
+function padZero(num) {
+  return (num < 10) ? `0${num}` : num;
+}
