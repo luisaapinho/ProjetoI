94266088137ae6d54ef67d1ea5c4f9ae4e5a7b91
luisaapinho <40220114@esmad.ipp.pt>
2023-05-31 12:48:22 +0100

    file with the expositivefiles opening

--- js/views/workspaceView.js
+++ js/views/workspaceView.js
@@ -9,6 +9,12 @@ const closeBtn = document.querySelector("#closeBtn");
 const fileImage = document.querySelector("#fileImage");
 const imgForm = document.querySelector("#imgCodeForms");
 const closeBtnForms = document.querySelector("#closeBtnForms");
+const fileA=document.querySelector("#fileA");
+const imgFileA=document.querySelector("#imgFileA");
+const closeBtnFileA=document.querySelector("#closeBtnFileA");
+const fileB=document.querySelector("#fileB")
+const imgFileB=document.querySelector("#imgFileB");
+const closeBtnFileB=document.querySelector("#closeBtnFileB");
 
 trashCan.addEventListener("click", function() {
   trashOpen.style.display = 'flex'; // Show the bin
@@ -49,5 +55,36 @@ closeBtnForms.addEventListener("click", function() {
   trashOpen.style.display = 'flex'; //Opens the trashOpen where the file was
 });
 
+fileA.addEventListener("click", function(){
+  imgFileA.style.display = 'flex'; // Show the bin
+  imgFileA.classList.add("slideIn"); // Add the class to the opening animation
+  setTimeout(function() {
+    imgFileA.classList.remove("slideIn"); // Remove class after the animation
+  }, 200); // Duration of the animation in ms
+})
+
+closeBtnFileA.addEventListener("click",function(){
+   imgFileA.classList.add("animated-close"); // Add the class to the closing animation
+  setTimeout(function() {
+    imgFileA.style.display = 'none'; // Remove the element after the animation
+    imgFileA.classList.remove("animated-close"); // Remove class after the animation
+  }, 200); //Duration of the animation in ms
+})
+
+fileB.addEventListener("click", function(){
+  imgFileB.style.display = 'flex'; // Show the bin
+  imgFileB.classList.add("slideIn"); // Add the class to the opening animation
+  setTimeout(function() {
+    imgFileB.classList.remove("slideIn"); // Remove class after the animation
+  }, 200); // Duration of the animation in ms
+})
+
+closeBtnFileB.addEventListener("click",function(){
+  imgFileB.classList.add("animated-close"); // Add the class to the closing animation
+ setTimeout(function() {
+   imgFileB.style.display = 'none'; // Remove the element after the animation
+   imgFileB.classList.remove("animated-close"); // Remove class after the animation
+ }, 200); //Duration of the animation in ms
+})
 // Inicializa o plugin "Responsive Image Maps"
 $('img[usemap]').rwdImageMaps();
