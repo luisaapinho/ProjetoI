a7e29aa55d02cb548d14494a571426b6c87937c6
luisaapinho <40220114@esmad.ipp.pt>
2023-06-01 11:57:45 +0100

    game3 opening just missing the exercicise

--- js/views/workspaceView.js
+++ js/views/workspaceView.js
@@ -15,6 +15,18 @@ const closeBtnFileA=document.querySelector("#closeBtnFileA");
 const fileB=document.querySelector("#fileB")
 const imgFileB=document.querySelector("#imgFileB");
 const closeBtnFileB=document.querySelector("#closeBtnFileB");
+const closeBtnServer=document.querySelector("#closeBtnServer");
+const serverImg=document.querySelector("#serverImg");
+const fileC=document.querySelector("#fileC");
+const imgFileC=document.querySelector("#imgFileC");
+const closeBtnFileC=document.querySelector("#closeBtnFileC");
+const try1=document.querySelector("#Try1");
+const try2=document.querySelector("#Try2");
+const try3=document.querySelector("#Try3");
+const try4=document.querySelector("#Try4");
+const CorrectFileC=document.querySelector("#CorrectFileC");
+const imgFileCEx=document.querySelector("#imgFileCEx");
+const closeBtnFileCEx=document.querySelector("#closeBtnFileCEx");
 
 trashCan.addEventListener("click", function() {
   trashOpen.style.display = 'flex'; // Show the bin
@@ -86,5 +98,36 @@ closeBtnFileB.addEventListener("click",function(){
    imgFileB.classList.remove("animated-close"); // Remove class after the animation
  }, 200); //Duration of the animation in ms
 })
+
+fileC.addEventListener("click", function(){
+  imgFileC.style.display = 'flex'; // Show the bin
+  imgFileC.classList.add("slideIn"); // Add the class to the opening animation
+  setTimeout(function() {
+    imgFileC.classList.remove("slideIn"); // Remove class after the animation
+  }, 200); // Duration of the animation in ms
+})
+
+closeBtnFileC.addEventListener("click",function(){
+  imgFileC.classList.add("animated-close"); // Add the class to the closing animation
+ setTimeout(function() {
+  imgFileC.style.display = 'none'; // Remove the element after the animation
+   imgFileC.classList.remove("animated-close"); // Remove class after the animation
+ }, 200); //Duration of the animation in ms
+})
+
+CorrectFileC.addEventListener("click", function() {
+  imgFileC.classList.add("animated-close"); // Add the class to the closing animation
+  setTimeout(function() {
+    imgFileC.style.display = 'none'; // Remove the element after the animation
+    imgFileC.classList.remove("animated-close"); // Remove class after the animation
+    
+    imgFileCEx.style.display = 'flex';   //Opens the image where are the code needed to one of the games
+    
+    imgFileC.classList.add("slideIn"); // Add the class to the opening animation
+    setTimeout(function() {
+      imgFileC.classList.remove("slideIn"); // Remove class after the animation
+    }, 1000);  //Duration of the animation in ms
+  }, 200); // Duration of the closing animation
+});
 // Inicializa o plugin "Responsive Image Maps"
 $('img[usemap]').rwdImageMaps();
