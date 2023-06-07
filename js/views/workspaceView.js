f4dfa5fbbd38ae2ce01ea2f831ea84396717e09c
luisaapinho <40220114@esmad.ipp.pt>
2023-06-07 11:43:25 +0100

    game 4 opening and closening

--- js/views/workspaceView.js
+++ js/views/workspaceView.js
@@ -64,30 +64,60 @@ pause.addEventListener("click", function(event) {
   event.preventDefault(); // takes to the pauseScreen when clicked the windows button
   window.location.href = "../html/pauseScreen.html";
 });
+//Trash bin icon
 const trashCan = document.querySelector("#trashCan");
+//Image of the trash open 
 const trashOpen = document.querySelector("#imgTrashOpen");
+//the button that closes the trash open
 const closeBtn = document.querySelector("#closeBtn");
-const fileImage = document.querySelector("#fileImage");
+//the file that u need to click to open the photo where it has the code to game 4
+const fileImage = document.querySelector("#fileImageTrash");
+//Image where that is the code to game 4(Geometric forms)
 const imgForm = document.querySelector("#imgCodeForms");
+//The button to close the image where there are the geometric forms
 const closeBtnForms = document.querySelector("#closeBtnForms");
+//The icon of the file A, one option for the first game
 const fileA=document.querySelector("#fileA");
+//Image that shows when u click the file that takes u to the expositive part and the exercise part
 const imgFileA=document.querySelector("#imgFileA");
+//Close button of the image above
 const closeBtnFileA=document.querySelector("#closeBtnFileA");
+//Icon of the file B other option of the game 1
 const fileB=document.querySelector("#fileB")
+//The image that shows up when u click the file that takes u to the expositive part and the exercise part
 const imgFileB=document.querySelector("#imgFileB");
+//Close button of the file above
 const closeBtnFileB=document.querySelector("#closeBtnFileB");
-const closeBtnServer=document.querySelector("#closeBtnServer");
-const serverImg=document.querySelector("#serverImg");
+//Icon of the file C, that opens the third game
 const fileC=document.querySelector("#fileC");
+//The image that shows up when u click the file c, here it takes u to the challenge that u
+//need to make before entering the part where there are the expostive part and the exercise part
 const imgFileC=document.querySelector("#imgFileC");
+//Close button to the image above
 const closeBtnFileC=document.querySelector("#closeBtnFileC");
+//These are the files that are in the imgFileC, that u need to click to find
+//the one where are placed the expositive/exercise part
 const try1=document.querySelector("#Try1");
 const try2=document.querySelector("#Try2");
 const try3=document.querySelector("#Try3");
 const try4=document.querySelector("#Try4");
+//This is the correct file that leads u to the expositive/exercise part
 const CorrectFileC=document.querySelector("#CorrectFileC");
+//This is the image that opens when u open the correct file, is where the expostive/exercise part is 
+//to the third game
 const imgFileCEx=document.querySelector("#imgFileCEx");
+//this is the close button to the imgFileCEX
 const closeBtnFileCEx=document.querySelector("#closeBtnFileCEx");
+//Icon of the file D, that opens the forth game
+const fileD=document.querySelector("#fileD");
+//Image where there is the dare that u need to go tru to get to the expositive/exercise part
+const imgFileD=document.querySelector("#imgFileD");
+//Close button of the file above
+const closeBtnFileD=document.querySelector("#closeBtnFileD");
+//The dare that u need to make before entering the expositive/exercise part
+const challengeD=document.querySelector("#fileImgD");
+
+
 
 trashCan.addEventListener("click", function() {
   trashOpen.style.display = 'flex'; // Show the bin
@@ -190,5 +220,22 @@ CorrectFileC.addEventListener("click", function() {
     }, 1000);  //Duration of the animation in ms
   }, 200); // Duration of the closing animation
 });
+
+
+fileD.addEventListener("click", function(){
+  imgFileD.style.display = 'flex'; // Show the bin
+  imgFileD.classList.add("slideIn"); // Add the class to the opening animation
+  setTimeout(function() {
+    imgFileD.classList.remove("slideIn"); // Remove class after the animation
+  }, 200); // Duration of the animation in ms
+})
+
+closeBtnFileD.addEventListener("click",function(){
+  imgFileD.classList.add("animated-close"); // Add the class to the closing animation
+ setTimeout(function() {
+  imgFileD.style.display = 'none'; // Remove the element after the animation
+   imgFileD.classList.remove("animated-close"); // Remove class after the animation
+ }, 200); //Duration of the animation in ms
+})
 // Inicializa o plugin "Responsive Image Maps"
 $('img[usemap]').rwdImageMaps();
