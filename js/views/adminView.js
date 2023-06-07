5c2c9ac17402a8ef4604f14b0a680951c97fd579
Simão Freitas <40220191@esmad.ipp.pt>
2023-06-07 19:20:47 +0100

    Fixed deleting multiple users

--- js/views/adminView.js
+++ js/views/adminView.js
@@ -6,12 +6,8 @@ document.addEventListener("DOMContentLoaded", function () {
   // Get all users from the UserModel module
   const allUsers = User.getUsers();
 
-  // Get the first 7 users from the allUsers array
-  const selectedUsers = allUsers.slice(0, 7);
-
-
   // Call the renderTable function to display all users in the table
-  renderTable(selectedUsers);
+  renderTable(allUsers);
 });
 
 // Select the elements with IDs "adminHome" and "lupa"
